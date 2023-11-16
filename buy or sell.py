3e901c1935e2351955210d86a80d5abae969b541
limin0321 <147285602+limin0321@users.noreply.github.com>
2023-11-16 18:37:03 +0700

    Update buy or sell.py

--- buy or sell.py
+++ buy or sell.py
@@ -1,1 +1,8 @@
-
+myMoney = int(input("มีเงินเท่าไหร่ : "))
+uCan = myMoney * 0.5
+price = int(input("ราคาสินค้า : "))
+D_one = myMoney - price
+if D_one > uCan:
+    print("ซื้อได้นะงับ")
+else:
+    print("เก็บเงินดีกว่าเนอะ")
