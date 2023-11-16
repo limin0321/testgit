13976d17a14d843604e370a409ec8e35e4d12596
limin0321 <147285602+limin0321@users.noreply.github.com>
2023-11-16 18:50:20 +0700

    Update คำนวณเกรด.py

--- คำนวณเกรด.py
+++ คำนวณเกรด.py
@@ -1,1 +1,18 @@
-
+urGrade = int(input("ได้คะเเนนเก็บเท่าไหร่ : "))
+if urGrade >= 80:
+    print("ได้เกรด A จร้า")
+elif urGrade >= 75:
+    print("ได้เกรด B+ จร้า")
+elif urGrade >= 70:
+    print("ได้เกรด B จร้า")
+elif urGrade >= 65:
+    print("ได้เกรด C+ จร้า")
+elif urGrade >= 60:
+    print("ได้เกรด C จร้า")
+elif urGrade >= 55:
+    print("ได้เกรด D+ จร้า")
+elif urGrade >= 50:
+    print("ได้เกรด D จร้า")
+else:
+    print("ติด F!!!")
+    
